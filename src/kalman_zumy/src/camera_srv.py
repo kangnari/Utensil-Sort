5ff456101f9bbb6fd67d448950c7d747bb2ef860
Eugenia Kang <enarikang@gmail.com>
2015-11-16 23:45:10 -0800

    Update camera_srv to be callable twice

--- src/kalman_zumy/src/camera_srv.py
+++ src/kalman_zumy/src/camera_srv.py
@@ -1,43 +1,49 @@
 #!/usr/bin/env python
 import rospy
+import sys
 
 from sensor_msgs.msg import Image
 from kalman_zumy.srv import ImageSrv, ImageSrvResponse
 
-""" Provides the last snapshot taken from usb_cam_utensil
-to whatever node requests it. In our case, the image_process
-node will be the only node to directly associate with this node.
+""" Provides the last snapshot taken from the appropriate
+usb_cam to whatever node requests it. The image_process node
+should be the only node requesting images from usb_cam_utensil/image_raw.
+The main_control and path_planning nodes should be the only nodes
+requesting images from usb_cam_global.
 
 Taken directly from EE106A lab 4.
 """
 class ImgService:
-  #Callback for when an image is received
-  def imgReceived(self, message):
-    #Save the image in the instance variable
-    self.lastImage = message
+    def __init__(self, cam_name):
+        #Create an instance variable to store the last image received
+        self.lastImage = None;
 
-  #When another node calls the service, return the last image
-  def getLastImage(self, request):
-    #Return the last image
-    return ImageSrvResponse(self.lastImage)
+        #Initialize the node
+        rospy.init_node('cam_listener')
 
-  def __init__(self):
-    #Create an instance variable to store the last image received
-    self.lastImage = None;
+        #Subscribe to the image topic
+        rospy.Subscriber("/usb_cam_" + cam_name + "/image_raw", Image, self.imgReceived)
 
-    #Initialize the node
-    rospy.init_node('cam_listener')
+        #Create the service
+        rospy.Service('last_image', ImageSrv, self.getLastImage)
 
-    #Subscribe to the image topic
-    rospy.Subscriber("/usb_cam_utensil/image_raw", Image, self.imgReceived)
+    #Callback for when an image is received
+    def imgReceived(self, message):
+        #Save the image in the instance variable
+        self.lastImage = message
 
-    #Create the service
-    rospy.Service('last_image', ImageSrv, self.getLastImage)
+    #When another node calls the service, return the last image
+    def getLastImage(self, request):
+        #Return the last image
+        return ImageSrvResponse(self.lastImage)
 
-  def run(self):
-    rospy.spin()
+    def run(self):
+        rospy.spin()
 
 #Python's syntax for a main() method
 if __name__ == '__main__':
-  node = ImgService()
-  node.run()
+    if len(sys.argv) < 2:
+        print("Use: camera_srv.py [ utensil/global ]")
+        sys.exit()
+    node = ImgService(sys.argv[2])
+    node.run()
